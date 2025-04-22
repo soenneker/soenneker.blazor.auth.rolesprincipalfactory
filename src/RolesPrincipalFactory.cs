00a8084c5ad892f56507e47c4370c424e8c2b31f
Jake Soenneker <jake@soenneker.com>
2025-04-22 08:42:29 -0500

    misc

--- src/RolesPrincipalFactory.cs
+++ src/RolesPrincipalFactory.cs
@@ -4,11 +4,12 @@ using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 using Microsoft.AspNetCore.Components.WebAssembly.Authentication.Internal;
 using Soenneker.Blazor.Auth.RolesPrincipalFactory.Abstract;
 using Soenneker.Extensions.IIdentity;
+using Soenneker.Extensions.ValueTask;
 
 namespace Soenneker.Blazor.Auth.RolesPrincipalFactory;
 
 /// <inheritdoc cref="IRolesPrincipalFactory"/>
-public class RolesPrincipalFactory: AccountClaimsPrincipalFactory<RemoteUserAccount>, IRolesPrincipalFactory
+public sealed class RolesPrincipalFactory: AccountClaimsPrincipalFactory<RemoteUserAccount>, IRolesPrincipalFactory
 {
     public RolesPrincipalFactory(IAccessTokenProviderAccessor accessor) : base(accessor)
     {
@@ -16,7 +17,7 @@ public class RolesPrincipalFactory: AccountClaimsPrincipalFactory<RemoteUserAcco
 
     public override async ValueTask<ClaimsPrincipal> CreateUserAsync(RemoteUserAccount account, RemoteAuthenticationUserOptions options)
     {
-        ClaimsPrincipal user = await base.CreateUserAsync(account, options);
+        ClaimsPrincipal user = await base.CreateUserAsync(account, options).NoSync();
 
         user.Identity.AddRolesFromJobTitle();
 
